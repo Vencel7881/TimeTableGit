15934e5fdbd11370deddcd37d3822613b68f4544
Vencel7881 <vincentzcouch@gmail.com>
2017-12-22 19:27:37 +0100

    set up of day's TextViews

--- app/src/main/java/com/example/vencel/timetablegit/MainActivity.java
+++ app/src/main/java/com/example/vencel/timetablegit/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.vencel.timetablegit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mondayTextView;
+    TextView tuesdayTextView;
+    TextView wednesdayTextView;
+    TextView thursdayTextView;
+    TextView fridayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.days_layout);
+
+        mondayTextView = (TextView) findViewById(R.id.MondayTextView);
+        tuesdayTextView = (TextView) findViewById(R.id.TuesdayTextView);
+        wednesdayTextView = (TextView) findViewById(R.id.WednesdayTextView);
+        thursdayTextView = (TextView) findViewById(R.id.ThursdayTextView);
+        fridayTextView = (TextView) findViewById(R.id.FridayTextView);
     }
 }
